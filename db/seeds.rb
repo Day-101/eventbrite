b550c0953e7b64d8afc201dedb82592f96f8fb79
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-02-16 23:44:54 +0100

    v2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,28 @@
 User.destroy_all
-5.times do |index|
-  User.create!(first_name: Faker::Games::Overwatch.hero,
-               last_name: Faker::Games::Overwatch.location,
-               email: "user#{index}" + "@yopmail.com",
-               password: "000000",
-               description: Faker::Lorem.paragraph_by_chars(number: rand(20..50)))
+10.times do |i|
+  User.create!(
+    first_name: Faker::Name.first_name  ,
+    last_name: Faker::Name.last_name  ,
+    email: "user#{i}" + "@yopmail.com",
+    password: "000000",
+    description: Faker::Lorem.paragraph_by_chars(number: rand(20..50)))
 end
 
 Event.destroy_all
 10.times do
-  Event.create!(start_date: Faker::Date.between(from: "2021-02-20", to: "2021-09-25"),
-                duration: [30, 60, 90, 120, 180, 210].sample,
-                title: Faker::Lorem.paragraph_by_chars(number: rand(5..140)),
-                description: Faker::Lorem.paragraph_by_chars(number: rand(20..1000)),
-                price: rand(1..1000),
-                location: Faker::Address.city,
-                host: User.all.sample)
+  Event.create!(
+    title: Faker::TvShows::SiliconValley.invention,
+    description: Faker::Lorem.paragraph_by_chars(number: rand(100..500)),
+    start_date: Faker::Date.between(from: "2021-03-01", to: "2021-03-31"),
+    duration: [30, 60, 90, 120].sample,
+    price: rand(1..1000),
+    location: Faker::Address.city,
+    host: User.all.sample)
 end
 
 Attendance.destroy_all
 10.times do
-  Attendance.create!(user: User.all.sample,
-                     event: Event.all.sample)
+  Attendance.create!(
+    event: Event.all.sample)
+    user: User.all.sample,
 end
